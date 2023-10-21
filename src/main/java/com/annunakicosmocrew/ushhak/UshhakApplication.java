7ab4df64b94ef923cb54f62f3f1ec82dbb1edbbc
Mertugrul <merty.ertugrul@gmail.com>
2023-10-21 01:14:07 +0100

    feat: first dependencies

---
+++ src/main/java/com/annunakicosmocrew/ushhak/UshhakApplication.java
@@ -0,0 +1,13 @@
+package com.annunakicosmocrew.ushhak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UshhakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UshhakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/annunakicosmocrew/ushhak/UshhakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annunakicosmocrew.ushhak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UshhakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
